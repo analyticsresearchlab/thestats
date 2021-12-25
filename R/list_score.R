d6fc8d43a529d6d316bb947e90a6c26d6c68f7e1
Olgun AYDIN <olgunaydinn@gmail.com>
2021-12-25 21:01:33 +0100

    if statement logical error has been solved

--- R/list_score.R
+++ R/list_score.R
@@ -29,48 +29,48 @@ list_score <- function(region_names = "all", city_names = "all", university_name
     scores_selected_tr <- subset(scores,select = var_ids)
     scores <- data.frame(depts,scores_selected_tr)
   }
-    if(region_names == "all" && city_names== "all" && university_names == "all" && department_names == "all")
+    if("all" %in% region_names && "all" %in% city_names && "all" %in% university_names && "all" %in% department_names)
     {
       result <- scores
     }
-    else if(region_names != "all" && city_names== "all" && university_names == "all" && department_names == "all")
+    else if(!"all" %in% region_names && "all" %in% city_names && "all" %in% university_names && "all" %in% department_names)
     {
       regions_unis <- filter(regions_cities, region %in% region_names)
       filtered_depts <- filter(scores, id %in% regions_unis$id)
       filtered_dept_cols <- colnames(filtered_depts)
       result <- filtered_depts
     }
-    else if(region_names == "all" && city_names!= "all" && university_names == "all" && department_names == "all")
+    else if("all" %in% region_names && !"all" %in% city_names && "all" %in% university_names && "all" %in% department_names)
     {
       cities_unis <- filter(regions_cities, city %in% city_names)
       filtered_depts <- filter(scores, id %in% cities_unis$id)
       result <- filtered_depts
     }
-    else if(region_names == "all" && city_names == "all" && university_names != "all" && department_names == "all")
+    else if("all" %in% region_names && "all" %in% city_names && !"all" %in% university_names && "all" %in% department_names)
     {
       filtered_depts <- filter(scores, university %in% university_names)
       result <- filtered_depts
     }
-    else if(region_names == "all" && city_names == "all" && university_names == "all" && department_names != "all")
+    else if("all" %in% region_names && "all" %in% city_names && "all" %in% university_names && !"all" %in% department_names)
     {
       filtered_depts <- filter(scores, grepl(department_names, department, fixed = TRUE))
       result <- filtered_depts
     }
-    else if(region_names != "all" && city_names== "all" && university_names == "all" && department_names != "all")
+    else if(!"all" %in% region_names && "all" %in% city_names && "all" %in% university_names && !"all" %in% department_names)
     {
       regions_unis <- filter(regions_cities, region %in% region_names)
       filtered_depts <- filter(scores, id %in% regions_unis$id)
       filtered_depts <- filter(filtered_depts, grepl(department_names, department, fixed = TRUE))
       result <- filtered_depts
     }
-    else if(region_names == "all" && city_names != "all" && university_names == "all" && department_names != "all")
+    else if("all" %in% region_names && !"all" %in% city_names && "all" %in% university_names && !"all" %in% department_names)
     {
       cities_unis <- filter(regions_cities, city %in% city_names)
       filtered_depts <- filter(scores, id %in% cities_unis$id)
       filtered_depts <- filter(filtered_depts, grepl(department_names, department, fixed = TRUE))
       result <- filtered_depts
     }
-    else if(region_names == "all" && city_names != "all" && university_names == "all" && department_names != "all")
+    else if("all" %in% region_names && !"all" %in% city_names && "all" %in% university_names && !"all" %in% department_names)
     {
       filtered_depts <- filter(scores, grepl(department_names, department, fixed = TRUE))
       result <- filtered_depts
